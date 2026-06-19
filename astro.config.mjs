6bb547c9d5ad74e6082f13ac12f931183c26212f
lsc5061 <lsc@dspaut.com>
2026-06-19 16:16:42 +0900

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 4322, // Set to 4322 to prevent port collision with main (3000) and support (4321)
+  }
+});
