1b78857ee05fed12c3a077eb3cf9e146f3b52f8c
lsc5061 <lsc@dspaut.com>
2026-06-29 13:57:26 +0900

    SEO 구글 엔진 관련 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://download.dspaut.com',
   server: {
     port: 4322, // Set to 4322 to prevent port collision with main (3000) and support (4321)
-  }
+  },
+  integrations: [sitemap()]
 });
